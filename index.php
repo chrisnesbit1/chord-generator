bb2ae5fa8eade4ecf4dcac4274c8dda3838a973e
Chris Nesbit <chris.nesbit1@gmail.com>
2017-03-14 16:08:45 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+/* **********************
+ *
+ * Chord Generator
+ *
+ * *********************/
+
+require __DIR__ . '/vendor/autoload.php';
+
+
+// Use / for the main/index page.
+get('/', function(){
+    echo "Front page";
+});
+?>
